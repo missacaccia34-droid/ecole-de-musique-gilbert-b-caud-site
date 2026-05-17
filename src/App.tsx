35cb48c484051a84041b1533c5300ea054db6d47
missacaccia34-droid <miss.acaccia34@gmail.com>
2026-05-17 10:48:23 +0100

    Add new SEO pages and update routing

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,16 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Index from "./pages/Index";
 import MentionsLegales from "./pages/MentionsLegales";
 import NotFound from "./pages/NotFound";
+
+// NOUVELLES PAGES SEO
+import Piano from "./pages/Piano";
+import Chant from "./pages/Chant";
+import Guitare from "./pages/Guitare";
+import EveilMusical from "./pages/EveilMusical";
 
 const queryClient = new QueryClient();
 
@@ -14,12 +21,43 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
+
       <BrowserRouter>
         <Routes>
+
+          {/* HOME */}
           <Route path="/" element={<Index />} />
-          <Route path="/mentions-legales" element={<MentionsLegales />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+
+          {/* SEO PAGES */}
+          <Route
+            path="/cours-piano-saint-gilles"
+            element={<Piano />}
+          />
+
+          <Route
+            path="/cours-chant-saint-gilles"
+            element={<Chant />}
+          />
+
+          <Route
+            path="/cours-guitare-saint-gilles"
+            element={<Guitare />}
+          />
+
+          <Route
+            path="/eveil-musical-saint-gilles"
+            element={<EveilMusical />}
+          />
+
+          {/* LEGAL */}
+          <Route
+            path="/mentions-legales"
+            element={<MentionsLegales />}
+          />
+
+          {/* 404 */}
           <Route path="*" element={<NotFound />} />
+
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
